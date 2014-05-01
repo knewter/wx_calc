fdcd792b23bb0721746694482c21b0073d4a67d2
Josh Adams <josh@isotope11.com>
2014-04-30 22:24:35 -0500

    Implement calculator server/logic

---
+++ lib/wx_calc/calculator_server.ex
@@ -0,0 +1,54 @@
+defmodule WxCalc.CalculatorServer do
+  use ExActor.GenServer
+
+  definit do: initial_state(%{display: "0", numbers: [], value: 0, current_operation: :noop})
+
+  defcall get_display, state: state, do: reply(state.display)
+
+  defcast number(num), state: state do
+    new_numbers = state.numbers ++ [num]
+    new_state(%{state | numbers: new_numbers, display: Enum.join(new_numbers)})
+  end
+
+  defcast add, state: state do
+    update_state(state, :addition) |> new_state
+  end
+
+  defcast subtract, state: state do
+    update_state(state, :subtraction) |> new_state
+  end
+
+  defcast multiply, state: state do
+    update_state(state, :multiplication) |> new_state
+  end
+
+  defcast divide, state: state do
+    update_state(state, :division) |> new_state
+  end
+
+  defcast equals, state: state do
+    update_state(state, :noop) |> new_state
+  end
+
+  def update_state(state, new_operation) do
+    entered_number = get_entered_number(state.numbers)
+    new_value = get_new_value(state.value, entered_number, state.current_operation)
+    new_display = "#{new_value}"
+    %{state | numbers: [], display: new_display, value: new_value, current_operation: new_operation}
+  end
+
+  def get_entered_number(numbers) do
+    {entered_number, _rest} = numbers |> Enum.join |> Integer.parse
+    entered_number
+  end
+
+  def get_new_value(current_value, number, operation) do
+    case operation do
+      :noop     -> number
+      :addition -> current_value + number
+      :subtraction -> current_value - number
+      :multiplication -> current_value * number
+      :division -> current_value / number
+    end
+  end
+end
